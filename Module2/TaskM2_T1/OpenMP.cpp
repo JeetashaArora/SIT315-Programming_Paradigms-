a16c6f2eedb7498d3336cc6e78c29f279be3231c
JeetashaArora <140963524+JeetashaArora@users.noreply.github.com>
2024-02-20 14:47:15 +0530

    Create OpenMP.cpp

---
+++ Module2/TaskM2_T1/OpenMP.cpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include<bits/stdc++.h>
+using namespace std::chrono;
+using namespace std;
+#define SIZE 200
+void multiply (int mat1[][SIZE], int mat2[][SIZE],int R1,int C1,int R2, int C2)
+{
+  //Storing the start time
+  auto start = high_resolution_clock::now();
+  int r_mat[R1][C2];
+  cout << "Multiplying the two given matrices" << endl;
+  #pragma omp parallel for
+  for (int i = 0; i < R1; i++)
+	{
+	  for (int j = 0; j < C2; j++)
+		{
+		  r_mat[i][j] =0;
+		  for (int k = 0; k < R2; k++)
+			{
+			  r_mat[i][j] += mat1[i][k] * mat2[k][j];
+			}
+		  //cout << r_mat[i][j] << "\t";
+		}
+	//cout << endl;
+	}
+  cout<<"=============================="<<endl;
+  //Storing the stop time
+  auto stop = high_resolution_clock::now();
+
+  //Calculating the time taken
+  //by subtracting the start time from the stop time
+  auto duration = duration_cast<milliseconds>(stop - start);
+
+  //Printing the time taken in milliseconds
+  cout << "Time taken for Parallel execution using OpenMP :" << duration.count() << " milliseconds"
+       << endl;
+}
+       
+//   ofstream fout;
+//   fout.open("OpenMP.txt");
+//   for (int i = 0; i < SIZE; i++) {
+//         for (int j = 0; j < SIZE; j++) 
+//         fout << r_mat[i][j] << " ";        
+//         fout << endl;
+//   }
+//   fout.close();
+//}
+
+int main ()
+{
+  Menu:
+  int R1=SIZE,R2=SIZE,C1=SIZE,C2=SIZE;
+  
+  int mat1[SIZE][SIZE];
+  int mat2[SIZE][SIZE];
+  
+  for(int i=0;i<R1;i++)
+  {
+      for(int j=0;j<C1;j++)
+      {
+          mat1[i][j]=rand()%100;
+      }
+  }
+  for(int i=0;i<R2;i++)
+  {
+      for(int j=0;j<C2;j++)
+      {
+          mat2[i][j]=rand()%100;
+      }
+  }
+  multiply (mat1, mat2,R1,C1,R2,C2);
+  return 0;
+}
+
+
+
+
+
+
